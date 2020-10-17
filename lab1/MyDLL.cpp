99e042283c60d43fa9cabffe286cd73a54172202
Ta3ik <snoopliner@gmail.com>
2020-10-17 16:39:45 +0700

    cur ver

--- lab1/MyDLL.cpp
+++ lab1/MyDLL.cpp
@@ -4,3 +4,203 @@
 
 #include "MyDLL.h"
 
+LinkedList::iterator::iterator() {
+
+}
+
+LinkedList::iterator &LinkedList::iterator::operator=(const LinkedList::iterator &other) {
+
+}
+
+
+bool LinkedList::iterator::operator!=(const iterator &other) const {
+
+}
+
+bool LinkedList::iterator::operator==(const LinkedList::iterator &other) const {
+
+}
+
+value_type &LinkedList::iterator::operator*() {
+
+}
+
+value_type *LinkedList::iterator::operator->() {
+
+}
+
+LinkedList::iterator &LinkedList::iterator::operator++() {
+
+}
+
+LinkedList::iterator LinkedList::iterator::operator++(int) {
+
+}
+
+LinkedList::iterator &LinkedList::iterator::operator--() {
+
+}
+
+LinkedList::iterator LinkedList::iterator::operator--(int) {
+
+}
+
+LinkedList::const_iterator::const_iterator() {
+
+}
+
+LinkedList::const_iterator &LinkedList::const_iterator::operator=(const LinkedList::const_iterator &other) {
+
+}
+
+bool LinkedList::const_iterator::operator!=(const LinkedList::const_iterator &other) const {
+
+}
+
+bool LinkedList::const_iterator::operator==(const LinkedList::const_iterator &other) const {
+
+}
+
+const value_type &LinkedList::const_iterator::operator*() const {
+
+}
+
+const value_type *LinkedList::const_iterator::operator->() const {
+
+}
+
+LinkedList::const_iterator &LinkedList::const_iterator::operator++() {
+
+}
+
+LinkedList::const_iterator LinkedList::const_iterator::operator++(int) {
+
+}
+
+LinkedList::const_iterator &LinkedList::const_iterator::operator--() {
+
+}
+
+LinkedList::const_iterator LinkedList::const_iterator::operator--(int) {
+
+}
+
+LinkedList::LinkedList() {
+
+}
+
+LinkedList::LinkedList(const LinkedList &other) {
+
+}
+
+LinkedList::LinkedList(LinkedList &&other) {
+
+}
+
+LinkedList::~LinkedList() {
+
+}
+
+LinkedList &LinkedList::operator=(const LinkedList &other) {
+
+}
+
+LinkedList &LinkedList::operator=(LinkedList &&other) {
+
+}
+
+LinkedList::iterator LinkedList::begin() {
+
+}
+
+LinkedList::const_iterator LinkedList::begin() const {
+
+}
+
+LinkedList::const_iterator LinkedList::cbegin() const {
+
+}
+
+LinkedList::iterator LinkedList::end() {
+
+}
+
+LinkedList::const_iterator LinkedList::end() const {
+
+}
+
+LinkedList::const_iterator LinkedList::cend() const {
+
+}
+
+int LinkedList::size() const {
+
+}
+
+bool LinkedList::empty() const {
+
+}
+
+value_type &LinkedList::front() {
+
+}
+
+const value_type &LinkedList::front() const {
+
+}
+
+value_type &LinkedList::back() {
+
+}
+
+const value_type &LinkedList::back() const {
+
+}
+
+LinkedList::iterator LinkedList::erase(LinkedList::iterator position) {
+
+}
+
+LinkedList::iterator LinkedList::erase(LinkedList::iterator begin, LinkedList::iterator end) {
+
+}
+
+int LinkedList::remove(const value_type &value) {
+
+}
+
+void LinkedList::clear() {
+
+}
+
+void LinkedList::pop_back() {
+
+}
+
+void LinkedList::pop_front() {
+
+}
+
+void LinkedList::push_back(const value_type &value) {
+
+}
+
+void LinkedList::push_front(const value_type &value) {
+
+}
+
+LinkedList::iterator LinkedList::insert(LinkedList::iterator before, const value_type &value) {
+
+}
+
+LinkedList &LinkedList::operator+=(const LinkedList &other) {
+
+}
+
+bool operator!=(const LinkedList &left, const LinkedList &right) {
+
+}
+
+bool operator==(const LinkedList &left, const LinkedList &right) {
+
+}
--- lab1/MyDLL.h
+++ lab1/MyDLL.h
@@ -5,59 +5,145 @@
 #ifndef LAB1SEM3_MYDLL_H
 #define LAB1SEM3_MYDLL_H
 
+typedef int value_type;
 
-#include <iostream>
 
-template<typename DATA>
 
-class MyDLL{
-protected:
-    typedef struct _LL;
+class LinkedList {
+private:
+    struct _List{
+        struct _List* next;
+        struct _List* prev;
+        value_type value;
+        _List();
+        explicit _List(value_type &value);
+        explicit _List(const value_type &value);
+    };
+
+    _List* endOfList;
+    int _size = 0;
+
 
 public:
-    struct _LL* head;
-    int size;
 
-protected:
-    typedef struct _LL{
-        struct _LL* _next;
-        struct _LL* _prev;
-        DATA _data;
-    }_LL;
+    class iterator {
 
-public:
-    bool isEmpty();
-    void push_back(const DATA & value);
-    void push_front(const DATA & value);
-    void pop_back();
-    void pop_fronf();
+        friend class LinkedList;
+        _List* node;
+
+        explicit iterator(_List el);
+
+    public:
+        iterator();
+        iterator & operator=(const iterator & other);
+        bool operator!=(const iterator & other) const;
+        bool operator==(const iterator & other) const;
+        //Возвращает ссылку на текущий элемент коллекции.
+        value_type & operator*();
+        //Возвращает указатель на текущий элемент коллекции.
+        value_type * operator->();
+
+        iterator & operator++();
+        iterator operator++(int);
+        iterator & operator--();
+        iterator operator--(int);
+    };
+    class const_iterator {
+
+        friend class LinkedList;
+        const _List* node;
+
+        explicit const_iterator(_List el);
+    public:
+        const_iterator();
+        const_iterator & operator=(const const_iterator & other);
+        bool operator!=(const const_iterator & other) const;
+        bool operator==(const const_iterator & other) const;
+        //Возвращает ссылку на текущий элемент коллекции.
+        const value_type & operator*() const;
+        //Возвращает указатель на текущий элемент коллекции.
+        const value_type * operator->() const;
+
+        const_iterator & operator++();
+        const_iterator operator++(int);
+        const_iterator & operator--();
+        const_iterator operator--(int);
+    };
+    /* Конструкторы */
+    LinkedList();
+    LinkedList(const LinkedList & other);
+    LinkedList(LinkedList && other);
+
+    /* Деструктор */
+    ~LinkedList();
+
+    /* Оператор присваивания */
+    LinkedList & operator=(const LinkedList & other);
+    LinkedList & operator=(LinkedList && other);
+
+    /* Доступ к итераторам */
+    //Возвращает итератор, указывающий на первый элемент списка.
+    iterator begin();
+    const_iterator begin() const;
+    const_iterator cbegin() const;
+    //Возвращает итератор, указывающий на элемент списка, следующий за последним
+    //т.е. отсутствующий в списке.
+    iterator end();
+    const_iterator end() const;
+    const_iterator cend() const;
+
+    /* Размерность */
+    //Возвращает размер списка.
+    int size() const;
+    //Возвращает истину, если список пуст.
+    bool empty() const;
+
+    /* Доступ к элементам */
+    //Возвращает ссылку на первый элемент списка.
+    value_type & front();
+    const value_type & front() const;
+    //Возвращает ссылку на последний элемент списка.
+    value_type & back();
+    const value_type & back() const;
+
+    /* Модификаторы */
+    //Удаляет элемент, на который указывает итератор pos.
+    iterator erase(iterator position);
+    //Удаляет элементы в интервале [begin, end).
+    iterator erase(iterator begin, iterator end);
+    //Удаляет все вхождения value в список.
+    int remove(const value_type & value);
+    //Очищает список.
     void clear();
 
+    //Удаляет последний элемент списка.
+    void pop_back();
+    //Удаляет первый элемент списка.
+    void pop_front();
+    //Добавляет значение value в конец списка.
+    void push_back(const value_type & value);
+    //Добавляет значение value в начало списка.
+    void push_front(const value_type & value);
+    //Вставляет значение value перед элементом, на который указывает before
+    iterator insert(iterator before, const value_type & value);
 
-    class iterator;
-    iterator begin() const;
-    iterator end() const;
-    iterator find(const DATA &) const;
-    iterator erase(iterator position);
-    iterator erase(iterator begin, iterator end);
-    iterator insert(iterator before, const DATA & value);
+    /* Операторы внутренние */
+    //Присоединяет other к списку.
+    LinkedList & operator+=(const LinkedList & other);
 
+    /* Операторы внешние */
+    friend bool operator!=(const LinkedList & left, const LinkedList & right);
+    friend bool operator==(const LinkedList & left, const LinkedList & right);
 
-    class iterator{
-    public:
-        explicit iterator(_LL* node);
-        _LL* node() const;
-        DATA* val() const;
-        iterator& operator++();
-        iterator& operator--();
-        iterator& operator++(int);
-        iterator& operator--(int);
-        bool operator ==(const iterator&) const;
-        bool operator !=(const iterator&) const;
-    private:
-        _LL* m_pNode;
-    };
 };
+
+/* Операторы внешние */
+//Сравнивает 2 листа
+bool operator!=(const LinkedList & left, const LinkedList & right);
+bool operator==(const LinkedList & left, const LinkedList & right);
+
+//Возвращает лист объединяющий 2 листа.
+LinkedList operator+(const LinkedList & left, const LinkedList & right);
 
 
 #endif //LAB1SEM3_MYDLL_H
