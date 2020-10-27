d19aad6514d04275f74fb4c34aae0e9d125e160d
Ta3ik <snoopliner@gmail.com>
2020-10-27 11:59:10 +0700

    add cmake_debug

---
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Ta3ik/CLionProjects/lab1/MyDll.cpp" "C:/Users/Ta3ik/CLionProjects/lab1/cmake-build-debug/CMakeFiles/lab1.dir/MyDll.cpp.obj"
+  "C:/Users/Ta3ik/CLionProjects/lab1/_List.cpp" "C:/Users/Ta3ik/CLionProjects/lab1/cmake-build-debug/CMakeFiles/lab1.dir/_List.cpp.obj"
+  "C:/Users/Ta3ik/CLionProjects/lab1/catch.cpp" "C:/Users/Ta3ik/CLionProjects/lab1/cmake-build-debug/CMakeFiles/lab1.dir/catch.cpp.obj"
+  "C:/Users/Ta3ik/CLionProjects/lab1/const_iterator.cpp" "C:/Users/Ta3ik/CLionProjects/lab1/cmake-build-debug/CMakeFiles/lab1.dir/const_iterator.cpp.obj"
+  "C:/Users/Ta3ik/CLionProjects/lab1/iterator.cpp" "C:/Users/Ta3ik/CLionProjects/lab1/cmake-build-debug/CMakeFiles/lab1.dir/iterator.cpp.obj"
+  "C:/Users/Ta3ik/CLionProjects/lab1/main.cpp" "C:/Users/Ta3ik/CLionProjects/lab1/cmake-build-debug/CMakeFiles/lab1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/MyDll.cpp.obj"
+  "CMakeFiles/lab1.dir/_List.cpp.obj"
+  "CMakeFiles/lab1.dir/catch.cpp.obj"
+  "CMakeFiles/lab1.dir/const_iterator.cpp.obj"
+  "CMakeFiles/lab1.dir/iterator.cpp.obj"
+  "CMakeFiles/lab1.dir/main.cpp.obj"
+  "lab1.exe"
+  "lab1.exe.manifest"
+  "lab1.pdb"
+  "liblab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
